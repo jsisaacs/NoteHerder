540dfa9f7da2b315df8d9cbb225cce477357147b
Josh Isaacson <jsisaacs@iu.edu>
2018-05-23 23:59:21 -0400

    Added Firebase functionalily via Rebase

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,18 @@ import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
+var Rebase = require('re-base');
+var firebase = require('firebase');
+var app = firebase.initializeApp({
+  apiKey: "AIzaSyCaS4NBj9CAgHH_F1f--oHHCqoZJPl8TVY",
+  authDomain: "noteherder-87094.firebaseapp.com",
+  databaseURL: "https://noteherder-87094.firebaseio.com",
+  projectId: "noteherder-87094",
+  storageBucket: "",
+  messagingSenderId: "1096092326000"
+});
+var base = Rebase.createClass(app.database());
+
 class Main extends React.Component {
   constructor() {
     super()
@@ -13,17 +25,26 @@ class Main extends React.Component {
     }
   }
 
-  componentWillMount() {
-    localStorage.getItem('notes') && this.setState({
-      currentNote: JSON.parse(localStorage.getItem('currentNote')),
-      notes: JSON.parse(localStorage.getItem('notes'))
-    })
-  }
+  // componentWillMount() {
+  //   localStorage.getItem('notes') && this.setState({
+  //     currentNote: JSON.parse(localStorage.getItem('currentNote')),
+  //     notes: JSON.parse(localStorage.getItem('notes'))
+  //   })
 
-  componentWillUpdate(nextProps, nextState) {
-    localStorage.setItem('notes', JSON.stringify(nextState.notes))
-    localStorage.setItem('currentNote', JSON.stringify(nextState.currentNote))
+  // }
+
+  // componentWillUpdate(nextProps, nextState) {
+  //   localStorage.setItem('notes', JSON.stringify(nextState.notes))
+  //   localStorage.setItem('currentNote', JSON.stringify(nextState.currentNote))
     
+  // }
+
+  componentDidMount() {
+    base.syncState(`notes`, {
+      context: this,
+      state: 'notes',
+      asArray: true
+    });
   }
 
   blankNote = () => {
@@ -58,7 +79,7 @@ class Main extends React.Component {
     this.setState({ notes })
     this.setCurrentNote(note)
 
-    localStorage.setItem(note.id, notes)
+    // localStorage.setItem(note.id, notes)
   }
 
   deleteNote = (note) => {
